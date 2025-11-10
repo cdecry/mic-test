31e3b30fffd644f8f3e76edb454656813320a4b1
crystal / ue <mfihy1@gmail.com>
2025-11-09 20:18:03 -0500

    Initialize NDSP and mic

--- source/main.cpp
+++ source/main.cpp
@@ -4,13 +4,31 @@
 #include <malloc.h>
 #include <3ds.h>
 
-int main()
-{
+// todo: move into utils
+void ok (const char* msg){ printf("\x1b[32m[OK]\x1b[0m  %s\n", msg); }
+void err(const char* msg){ printf("\x1b[31m[!!]\x1b[0m %s\n", msg); }
+void inf(const char* msg){ printf("\x1b[36m[--]\x1b[0m %s\n", msg); }
+
+int main() {
   gfxInitDefault();
   consoleInit(GFX_BOTTOM, NULL);
+  inf("press start to exit\n");
 
-  while(aptMainLoop())
-  {
+  u32 micbuf_size = 0x30000;
+	// u32 micbuf_pos = 0;
+	u8* micbuf = static_cast<u8*>(memalign(0x1000, micbuf_size));
+
+  inf("initializing NDSP...");
+  if (R_FAILED(ndspInit())) {
+    err("failed to initialize NDSP.");
+  } else ok("NDSP initialized.");
+
+  inf("initializing mic...");
+  if (R_FAILED(micInit(micbuf, micbuf_size))) {
+    err("failed to initialize mic.");
+  } else ok("mic initialized.");
+
+  while(aptMainLoop()) {
     hidScanInput();
     gspWaitForVBlank();
 
@@ -22,6 +40,9 @@ int main()
     gfxSwapBuffers();
   }
 
+  micExit();
+  free(micbuf);
+  ndspExit();
   gfxExit();
   return 0;
 }
